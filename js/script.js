dc46b28dcdf6e07a7d775b4659578230eaa678fc
Boris Chepurnoy <beastbs.dev@gmail.com>
2023-01-17 21:38:24 +0200

    added slider

--- js/script.js
+++ js/script.js
@@ -295,4 +295,53 @@ window.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }, 4000);
   }
+
+  // Slider
+
+  const slides = document.querySelectorAll(".offer__slide"),
+    prev = document.querySelector(".offer__slider-prev"),
+    next = document.querySelector(".offer__slider-next");
+
+  const total = document.querySelector("#total"),
+    current = document.querySelector("#current");
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((slide) => (slide.style.display = "none"));
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slideIndex < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlide(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlide(-1);
+  });
+  next.addEventListener("click", () => {
+    plusSlide(1);
+  });
 });
