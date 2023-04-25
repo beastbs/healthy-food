5d40a4662ec8ba23c436586817e8b36295444a6b
Boris Chepurnoy <beastbs.dev@gmail.com>
2023-04-25 17:42:37 +0300

    added functionality for calculator

--- js/script.js
+++ js/script.js
@@ -306,10 +306,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const slidesWrapper = document.querySelector(".offer__slider-wrapper");
   const slidesField = document.querySelector(".offer__slider-inner");
   const widthSliderWrapper = window.getComputedStyle(slidesWrapper).width;
-  const widthInNumbers = +widthSliderWrapper.slice(
-    0,
-    widthSliderWrapper.length - 2
-  );
 
   const total = document.querySelector("#total"),
     current = document.querySelector("#current");
@@ -377,11 +373,15 @@ window.addEventListener("DOMContentLoaded", () => {
     dots[slideIndex - 1].classList.add("dot-active");
   };
 
+  function deleteNotDigit(string) {
+    return Number(string.replace(/\D/g, ""));
+  }
+
   next.addEventListener("click", () => {
-    if (offset === widthInNumbers * (slides.length - 1)) {
+    if (offset === deleteNotDigit(widthSliderWrapper) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += widthInNumbers;
+      offset += deleteNotDigit(widthSliderWrapper);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -392,9 +392,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
   prev.addEventListener("click", () => {
     if (offset === 0) {
-      offset = widthInNumbers * (slides.length - 1);
+      offset = deleteNotDigit(widthSliderWrapper) * (slides.length - 1);
     } else {
-      offset = offset - widthInNumbers;
+      offset = offset - deleteNotDigit(widthSliderWrapper);
     }
 
     slidesField.style.transform = `translateX(-${offset}px)`;
@@ -409,7 +409,7 @@ window.addEventListener("DOMContentLoaded", () => {
     if (target.classList.contains("dot")) {
       const slideTo = Number(target.getAttribute("data-slide-to"));
       slideIndex = slideTo;
-      offset = widthInNumbers * (slideTo - 1);
+      offset = deleteNotDigit(widthSliderWrapper) * (slideTo - 1);
 
       slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -417,4 +417,119 @@ window.addEventListener("DOMContentLoaded", () => {
       addActiveSlideDot();
     }
   });
+
+  showName("Igor");
+
+  // Calc
+
+  const resultColories = document.querySelector(".calculating__result span");
+  const sexLocalStorage = localStorage.getItem("sex");
+  const ratioLocalStorage = localStorage.getItem("ratio");
+
+  let ratio = ratioLocalStorage ? Number(ratioLocalStorage) : 1.55;
+  let sex = sexLocalStorage ? sexLocalStorage : "male";
+  let height, weight, age;
+
+  function calcTotalCalories() {
+    if (!height || !weight || !age) {
+      resultColories.innerText = "___";
+      return;
+    }
+
+    if (sex === "female") {
+      resultColories.innerText = Math.round(
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+      );
+    }
+
+    if (sex === "male") {
+      resultColories.innerText = Math.round(
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+      );
+    }
+  }
+  calcTotalCalories();
+
+  function getStaticInfo(
+    selector,
+    activeClass = "calculating__choose-item_active"
+  ) {
+    const elements = document.querySelectorAll(`${selector} div`);
+
+    document.querySelector(selector).addEventListener("click", (event) => {
+      if (event.target.getAttribute("data-ratio")) {
+        ratio = Number(event.target.getAttribute("data-ratio"));
+        localStorage.setItem("ratio", ratio);
+      } else {
+        sex = event.target.getAttribute("id");
+        localStorage.setItem("sex", sex);
+      }
+
+      if (event.target.id !== "gender" && event.target.id !== "ratio") {
+        elements.forEach((elem) => {
+          elem.classList.remove(activeClass);
+        });
+
+        event.target.classList.add(activeClass);
+      }
+
+      calcTotalCalories();
+    });
+  }
+  getStaticInfo("#gender");
+  getStaticInfo("#ratio");
+
+  function initLocalSettings(
+    selector,
+    activeClass = "calculating__choose-item_active"
+  ) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((elem) => {
+      elem.classList.remove(activeClass);
+
+      if (elem.getAttribute("id") === sex) {
+        elem.classList.add(activeClass);
+      }
+
+      if (Number(elem.getAttribute("data-ratio")) === ratio) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+  initLocalSettings("#gender div");
+  initLocalSettings("#ratio div");
+
+  function getDynamicInfo(selector) {
+    const inputField = document.querySelector(selector);
+
+    inputField.addEventListener("input", () => {
+      if (inputField.value.match(/\D/g)) {
+        inputField.style.boxShadow = "0px 4px 2px -2px #f00";
+      } else if (!inputField.value) {
+        inputField.style.boxShadow = "0 4px 15px rgba(0, 0, 0, 0.2)";
+      } else {
+        inputField.style.boxShadow = "0 4px 2px -2px #54ed39";
+      }
+      switch (inputField.getAttribute("id")) {
+        case "height":
+          height = Number(inputField.value);
+          break;
+        case "weight":
+          weight = Number(inputField.value);
+          break;
+        case "age":
+          age = Number(inputField.value);
+          break;
+      }
+      calcTotalCalories();
+    });
+  }
+  getDynamicInfo("#height");
+  getDynamicInfo("#weight");
+  getDynamicInfo("#age");
 });
+
+function deleteNotDigit(string) {
+  return Number(string.replace(/\D/g, ""));
+}
--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,18 @@
-export async function getCurrencyRates() {
-  const baseUrl = "";
+// export async function getCurrencyRates() {
+//   const baseUrl = "";
 
-  try {
-    const response = await fetch(baseUrl);
-    const toJson = await response.toJson();
-    return toJson;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+//   try {
+//     const response = await fetch(baseUrl);
+//     const toJson = await response.toJson();
+//     return toJson;
+//   } catch (error) {
+//     throw new Error(error.message);
+//   }
+// }
+
+function showName(name) {
+  console.log(`My name is ${name}`);
 }
+
+const result = deleteNotDigit("200%");
+console.log(result);
