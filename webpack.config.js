f257c1a8e48f517c1205efd01d3766fa7c355261
Boris Chepurnoy <beastbs.dev@gmail.com>
2023-04-30 10:45:23 +0300

    added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,27 @@ module.exports = {
   },
   watch: true,
   devtool: "source-map",
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|cower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  debug: true,
+                  corejs: 3,
+                  useBuiltIns: "usage",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
